7d3d91be8e94db95028b7f6376e34144703a031f
Christian Even <christian.even@outlook.com>
2020-05-28 16:20:48 +0200

    Mon app Django Girls, deuxième commit

--- monsite/urls.py
+++ monsite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
+    path('', views.accueil, name='accueil'),
     path('admin/', admin.site.urls),
 ]
